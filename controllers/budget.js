201e4577dd1389aaf03fefd69c74710f3a2dcfc5
nhalkogit config --global init.defaultBranch main <nata26564@gmail.com>
2025-04-10 19:27:25 -0500

    create-router

--- controllers/budget.js
+++ controllers/budget.js
@@ -5,61 +5,36 @@ const Income = require('../models/income');
 const Expense = require('../models/expense');
 
 
-// router.get('/', (req, res) => {
-//     res.send(' Welcome to Budget Buddy!');
-//   });
-  
 
-// add inc
-router.post("/budget/incomes/add", async (req, res) => {
-    const { userId, category, amount } = req.body;
+// crate 
+router.post("/budgets", verifyToken, async (req, res)=> {
+    const { name, amount, user } = req.body;
+ if (!name){
+    return res.status(400).json({error: "Name is required!"});
+ }
+ if (!amount){
+    return res.status(400).json({error: "Amount is required!"});
+ }
+ if (!user){
+    return res.status(400).json({error: "User is required!"});
+ }
 
-    if (!userId || !category || !amount) {
-        return res.status(400).json({ message: "Please provide all income fields" });
-    }
-
-    const newIncome = new Income({ userId, category, amount });
-    const savedIncome = await newIncome.save();
-
-    res.status(201).json({ message: "Income added", income: savedIncome });
+    try {
+const newBudget = new Budget({
+    name: name,
+    amount: amount,
+    user: user,
+    expenses: []
 });
-
-// edit 
-router.put("/budget/incomes/edit/:id", async (req, res) => {
-    const income = await Income.findByIdAndUpdate(req.params.id, req.body, { new: true });
-    res.json({ message: "Income updated", income });
+const savedBudget = await newBudget.save();
+res.status (201).json({
+    messege: "Created!"
+    budget: savedBudget
 });
-
-// del 
-router.delete("/budget/incomes/delete/:id", async (req, res) => {
-    await Income.findByIdAndDelete(req.params.id);
-    res.json({ message: "Income deleted" });
+}catch (error) {
+    res.status(500).json(error);
+}
 });
-
-
-// add exp
-router.post("/budget/expenses/add", async (req, res) => {
-    const { userId, category, amount } = req.body;
-
-    if (!userId || !category || !amount) {
-        return res.status(400).json({ message: "Please provide all expense fields" });
-    }
-
-    const newExpense = new Expense({ userId, category, amount });
-    const savedExpense = await newExpense.save();
-
-    res.status(201).json({ message: "Expense added", expense: savedExpense });
-});
-//edit
-router.put("/budget/expenses/edit/:id", async (req, res) => {
-    const expense = await Expense.findByIdAndUpdate(req.params.id, req.body, { new: true });
-    res.json({ message: "Expense updated", expense });
-});
-
-// del
-router.delete("/budget/expenses/delete/:id", async (req, res) => {
-    await Expense.findByIdAndDelete(req.params.id);
-    res.json({ message: "Expense deleted" });
-});
+ 
 
 module.exports = router;
