0fa6553f623081e57cff317f8fe29397cbc6462e
gerol-r <gerol.revills@gmail.com>
2025-04-21 18:07:30 -0400

    connecting to netlify

--- server.js
+++ server.js
@@ -1,36 +1,55 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const cors = require('cors');
-const logger = require('morgan');
+const mongoose = require("mongoose");
+const cors = require("cors");
+const logger = require("morgan");
 
 // Import routers
-const authRouter = require('./controllers/auth');
-const testJwtRouter = require('./controllers/test-jwt');
-const usersRouter = require('./controllers/users');
-const budgetRouter = require('./controllers/budget')
+const authRouter = require("./controllers/auth");
+const testJwtRouter = require("./controllers/test-jwt");
+const usersRouter = require("./controllers/users");
+const budgetRouter = require("./controllers/budget");
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI);
 
-mongoose.connection.on('connected', () => {
+mongoose.connection.on("connected", () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
 // Middleware
-app.use(cors());
+const allowedOrigins = [
+  "https://the-budget-buddy-app.netlify.app/sign-in",
+  "http://localhost:5173",
+];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      console.warn(`CORS blocked origin: ${origin}`);
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS", // Explicitly allow methods including OPTIONS
+  allowedHeaders:
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization", // Allow common headers + Authorization
+  credentials: true, // Allow credentials (cookies/auth headers)
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 app.use(express.json());
-app.use(logger('dev'));
+app.use(logger("dev"));
 
 // Routes
-app.use('/auth', authRouter);
-app.use('/test-jwt', testJwtRouter);
-app.use('/users', usersRouter);
-app.use('/budgets', budgetRouter);
+app.use("/auth", authRouter);
+app.use("/test-jwt", testJwtRouter);
+app.use("/users", usersRouter);
+app.use("/budgets", budgetRouter);
 
 // Start the server and listen on port 3000
 app.listen(3000, () => {
-  console.log('The express app is ready!');
+  console.log("The express app is ready!");
 });
